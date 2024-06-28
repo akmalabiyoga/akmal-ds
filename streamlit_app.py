72234cc54a879e924e3c2b4f60a8ab83c8f53ae0
akmalabiyoga <akmalabiyoga@gmail.com>
2024-06-28 06:57:29 +0000

    koko

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,47 @@
-import altair as alt
-import numpy as np
-import pandas as pd
-import streamlit as st
+import streamlit as st 
+import pandas as pd 
+from st_aggrid import AgGrid
 
-"""
-# Welcome to Streamlit!
+house = pd.read_csv('house_clean.csv')
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+def main() : 
+  ##### Untuk olah text
+  st.header('Halaman Streamlit Dwi A')
+  st.subheader('This is SubHeader')
+  st.markdown('# Rendering Markdown ')
+  st.write('Some Phytagorean Equation : ')
+  st.latex('c^2 = a^2+b^2')
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+  st.dataframe(house)
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+  ##### Untuk menulis metric
+  st.write('Metrics')
+  st.metric(label="Temperature", value="70 °F", delta="-1.2 °F")
+  
+  ##### Untuk menampilkan Grid -- masih blm tampil
+  st.write('Menampilkan Dataframe dengan St AgGrid')
+  #AgGrid(house)
+  
+  st.table([x for x in range(1,5)])
+  
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+  click_me_btn = st.button('Click Me')
+  st.write(click_me_btn) #Return True kalo di Click 
+  check_btn = st.checkbox('Klik Jika Setuju')
+  if check_btn :
+      st.write('Anda Setuju')
+  
+  radio_button= st.radio('Choose below',[x for x in range(1,3)])
+  st.write('Anda Memilih',radio_button)
+    
+  #slider 
+  age_slider = st.slider('Berapa Usia Anda',0,100)
+  st.write('Usia Anda',age_slider)
+    
+  #Input (Typing)
+  num_input = st.number_input('Input Berapapun')
+  st.write('Kuadrat dari {} adalah {}'.format(num_input,num_input**2))
+      
+if __name__ == '__main__' : 
+  main()
